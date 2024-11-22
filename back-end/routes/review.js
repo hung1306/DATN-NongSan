fb3582a0af9d7211f1c72cf93f012efbfb721f12
hung1306 <mycode13062002@gmail.com>
2024-11-22 08:48:26 +0700

    wip

--- back-end/routes/review.js
+++ back-end/routes/review.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const reviewController = require('../controllers/reviewController');
+const reviewController = require("../controllers/reviewController");
 
-router.post('/review', reviewController.addReview);
-router.get('/review/:productId', reviewController.getAllReviewByProductId);
-router.get('/review/rating/:productId', reviewController.getAmountOfReview);
+router.post("/review", reviewController.addReview);
+router.get("/review/:productId", reviewController.getAllReviewByProductId);
+router.get("/review/rating/:productId", reviewController.getAmountOfReview);
 
 module.exports = router;
